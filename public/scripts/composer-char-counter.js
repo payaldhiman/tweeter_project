99034721c6f2a13d8e184d5d109c3c8510674f2c
Payal Dhiman <payaldhiman@outlook.com>
2017-12-13 02:52:51 +0000

    completed creating tweets excercise

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+  $( ".new-tweet textarea" ).on( "keyup", function() {
+    var max = 140;
+    var len = $(this).val().length;
+    var char = max - len;
+    if (len > max) {
+      var color = "red";
+    } else {
+      var color = "black";
+    }
+    $('.counter').text(char).css({ color: color });
+
+  });
+});
